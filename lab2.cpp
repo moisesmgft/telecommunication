c5785d1f7c532a883372120363dc3559b4a684a4
moisesmgft <moisesmgft@gmail.com>
2024-05-07 15:56:22 -0300

    lab2 with new probs

--- lab2.cpp
+++ lab2.cpp
@@ -11,43 +11,35 @@
 using namespace std;
 
 int main() {
+
+    cout << fixed << setprecision(7);
     vector<int> sizes = {100,200,500,1000};
     unordered_map<int,vector<vector<int>>> edges;
+    unordered_map<int,vector<vector<int>>> inverted_edges;
     for(auto p : sizes) {
-        vector<vector<int>> local;
-        int num = p - (p % 7 <= 3 ? p % 7 : 7 - p % 7);
-        ldpc::create_graph(num, 3, 7, local);
+        vector<vector<int>> local, inv;
+        int num = p;
+        ldpc::create_graph(num, 3, 4, local);
+        ldpc::invert_graph(local, inv, num);
         edges[p] = local;
+        inverted_edges[p] = inv;
     }
     vector<double> probabilites = {1e-1,5e-2,2e-3,1e-3,5e-4,2e-5,1e-5};
 
     vector<bool> original;
-    bits::generate_bits(original, 10e3, 0.5);
-    for(auto p : probabilites) {
+    bits::generate_bits(original, 1e5, 0.5);
+    for(double p = 0.5; p >= 1e-5; p *= 0.9) {
         for(auto s : sizes) {
             vector<bool> encoded, encoded_error, decoded;
             double final_error;
-            ldpc::encode(original, edges[s],  s - (s % 7 <= 3 ? s % 7 : 7 - s % 7), encoded);
+            ldpc::encode(original, edges[s],  s, encoded);
             bits::communicate_bits_through_bsc(encoded, encoded_error, p);
-            ldpc::decode(encoded_error, edges[s],  s - (s % 7 <= 3 ? s % 7 : 7 - s % 7), decoded, s);
+            ldpc::decode(encoded_error, edges[s],  s, decoded, 50);
             final_error = bits::error_percentage(original, decoded);
 
             cout << p << "," << s << "," << final_error << "\n";
         }
     }
-
-    // int i = 0;
-    // for (auto b: input) 
-    //     cout << b << (++i % 4 == 0 ? "     " : "");
-    // cout << endl;s
-    // i = 0;
-    // for (auto b: out)
-    //     cout << b << (++i % 8 == 0 ? " " : "");
-    // cout << endl;
-    // i = 0;
-    // for (auto b: decoded)
-    //     cout << b << (++i % 4 == 0 ? "     " : "");
-    // cout << endl;
 
     return 0;
 
