1350f92c5f418f831ba05f3730cc0acae45e621b
Rafael450 <rafaelcamargo633@gmail.com>
2024-05-07 20:54:38 -0300

    chore: updated main

--- lab2.cpp
+++ lab2.cpp
@@ -18,23 +18,21 @@ int main() {
     unordered_map<int,vector<vector<int>>> inverted_edges;
     for(auto p : sizes) {
         vector<vector<int>> local, inv;
-        int num = p;
-        ldpc::create_graph(num, 3, 4, local);
+        int num = p-(p % 7 <= 3 ? p % 7 : (p % 7)-7);
+        ldpc::create_graph(num, 3, 7, local);
         // ldpc::invert_graph(local, inv, num);
         edges[p] = local;
         inverted_edges[p] = inv;
     }
     vector<double> probabilites = {1e-1,5e-2,2e-3,1e-3,5e-4,2e-5,1e-5};
 
-    vector<bool> original;
-    bits::generate_bits(original, 1e5, 0.5);
+    vector<bool> original(1e5,false);
     for(double p = 0.5; p >= 1e-5; p *= 0.9) {
         for(auto s : sizes) {
-            vector<bool> encoded, encoded_error, decoded;
+            vector<bool> encoded_error, decoded;
             double final_error;
-            ldpc::encode(original, edges[s],  s, encoded);
-            bits::communicate_bits_through_bsc(encoded, encoded_error, p);
-            ldpc::decode(encoded_error, edges[s],  s, decoded, 50);
+            bits::communicate_bits_through_bsc(original, encoded_error, p);
+            ldpc::decode(encoded_error, edges[s],  s-(s % 7 <= 3 ? s % 7 : (s % 7)-7), decoded, 50);
             final_error = bits::error_percentage(original, decoded);
 
             cout << p << "," << s << "," << final_error << "\n";
